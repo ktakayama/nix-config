b16aa579e0c1e7b43f6a180841477a408e103ebf
Kyosuke Takayama <loiseau@gmail.com>
2026-07-08 13:43:28 +0900

    feat: fix fvm version on 4.0.5

--- custom-osx.nix
+++ custom-osx.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 
+let
+  # fvm 4.0.5 固定用の nixpkgs スナップショット
+  pkgs-pinned = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/d33369954a67ae3322177dc9a3d564092912120c.tar.gz";
+    sha256 = "sha256-bG1LAS3YehMzp4RSXw99o3yMEO/Ktb0Tx29RCtEU0Tk=";
+  }) { system = pkgs.stdenv.hostPlatform.system; };
+in
 {
   home.homeDirectory = "/Users/takayama";
 
@@ -10,7 +17,7 @@
 
     # development
     swiftlint
-    fvm
+    pkgs-pinned.fvm # 4.0.5 固定
     rustup
     terraform-ls
 
